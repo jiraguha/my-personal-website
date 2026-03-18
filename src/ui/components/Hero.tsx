a0f96c7d43bb212442f0312600778d56081378b9
jpiraguha <jiraguha@gmail.com>
2026-03-18 21:41:39 +0100

    chore: auto-commit changes

--- src/ui/components/Hero.tsx
+++ src/ui/components/Hero.tsx
@@ -24,11 +24,14 @@ function GitHubIcon() {
 
 function ResumeIcon() {
   return (
-    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" aria-hidden="true">
-      <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
-      <polyline points="14 2 14 8 20 8" />
-      <line x1="12" y1="18" x2="12" y2="12" />
-      <line x1="9" y1="15" x2="15" y2="15" />
+    <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M6 2h12a2 2 0 0 1 2 2v16a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2z
+           M12 6a3 3 0 1 0 0 6 3 3 0 0 0 0-6z
+           M7 19a5 5 0 0 1 10 0z"
+      />
     </svg>
   );
 }
